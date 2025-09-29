0320f8bbb265161967ca08a3e56c192d42a340f4
Brayden Castro <bc3825@rit.edu>
2025-09-29 18:12:46 -0400

    Implment the controller for createneeds

--- ufund-api/src/main/java/com/ufund/api/ufundapi/Controller/CupboardContoller.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/Controller/CupboardContoller.java
@@ -1,10 +1,20 @@
 package com.ufund.api.ufundapi.Controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ufund.api.ufundapi.DAO.CupboardDAO;
+import com.ufund.api.ufundapi.Model.Need;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/cupboard")
@@ -12,14 +22,30 @@ public class CupboardContoller {
 
     //Constructor injection of DAO
     private final CupboardDAO cupboardDAO;
+    private static final Logger LOG = Logger.getLogger(CupboardContoller.class.getName());
 
     public CupboardContoller(CupboardDAO cupboardDAO) {
         this.cupboardDAO = cupboardDAO;
     }
     
 
+    @PostMapping("/need")
+    public ResponseEntity<Object> createNeed(@RequestBody Need need){
 
-    public ResponseEntity<Object> createNeed(){
-        
+        LOG.info("POST /cupboard/need " + need);
+        try {
+            //Check if need exist 
+            if(cupboardDAO.needExistByName(need.getName())){
+                LOG.info("Need with name '" + need.getName() + "' already exists");
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+            Need createNeed = cupboardDAO.addNeed(need);
+            
+            return new ResponseEntity<>(createNeed, HttpStatus.OK);
+            
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage(),e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
