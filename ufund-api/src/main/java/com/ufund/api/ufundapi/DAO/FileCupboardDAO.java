634c997bf56a7a5bf29e265c7fbffd354b54cd32
Brayden Castro <bc3825@rit.edu>
2025-09-30 15:20:05 -0400

    Added java doc for second constructor

--- ufund-api/src/main/java/com/ufund/api/ufundapi/DAO/FileCupboardDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/DAO/FileCupboardDAO.java
@@ -29,6 +29,13 @@ public class FileCupboardDAO implements CupboardDAO {
         this.file = new File(filePath);
         loadCupboard();
     }
+
+    /**
+     * Purpose of Unit test 
+     * @param filePath Path to the JSON file use for persistance. 
+     * @param objectMapper reading the JSON file 
+     * @throws IOException if an error occurs during file read/write 
+     */
     public FileCupboardDAO(String filePath,ObjectMapper objectMapper) throws IOException{
         this.objectMapper = objectMapper;
         this.file = new File(filePath);
