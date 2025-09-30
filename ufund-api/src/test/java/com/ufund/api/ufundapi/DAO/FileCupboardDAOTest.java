853c15b4a78707a39640bc988eaddce70a942e3e
nyork24 <nolanblox@gmail.com>
2025-09-30 13:56:13 -0400

    added unit tests to searchNeeds

--- ufund-api/src/test/java/com/ufund/api/ufundapi/DAO/FileCupboardDAOTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/DAO/FileCupboardDAOTest.java
@@ -2,11 +2,15 @@ package com.ufund.api.ufundapi.DAO;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -78,5 +82,47 @@ public class FileCupboardDAOTest {
         FileCupboardDAO reloadDAO = new FileCupboardDAO(tempfile.getAbsolutePath(), mapper);
         
         assertTrue(reloadDAO.needExistByName("Crayons"));
+    }
+
+    @Test
+    void testSearchNeeds() throws IOException{
+        Need need1 = new Need("Pens", 12,3.5);
+        Need need2 = new Need("Pencils", 15,4.5);
+        Need need3 = new Need("Markers", 10,4.5);
+
+        cupboardDAO.addNeed(need1);
+        cupboardDAO.addNeed(need2);
+        cupboardDAO.addNeed(need3);
+
+        //Recreate DAO using same file path
+        ObjectMapper mapper = new ObjectMapper();
+        FileCupboardDAO reloadDAO = new FileCupboardDAO(tempfile.getAbsolutePath(), mapper);
+        
+        List<Need> actual = reloadDAO.getNeedByName("Pen");
+        ArrayList<Need> expected = new ArrayList<>();
+        expected.add(need1);
+        expected.add(need2);
+
+        assertIterableEquals(
+            expected.stream().map(Need::getName).toList(),
+            actual.stream().map(Need::getName).toList()
+        );
+
+    }
+
+    @Test
+    void testSearchNeedsNoneFound() throws IOException{
+        Need need1 = new Need("Pens", 12,3.5);
+
+        cupboardDAO.addNeed(need1);
+
+        //Recreate DAO using same file path
+        ObjectMapper mapper = new ObjectMapper();
+        FileCupboardDAO reloadDAO = new FileCupboardDAO(tempfile.getAbsolutePath(), mapper);
+        
+        List<Need> actual = reloadDAO.getNeedByName("Markers");
+
+        assertEquals(actual.isEmpty(), true);
+
     }
 }
