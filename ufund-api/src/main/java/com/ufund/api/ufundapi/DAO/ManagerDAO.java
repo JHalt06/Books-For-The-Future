e76eebe9b865c4ecacbbf58f71f99c4321c2a972
JHalt06 <jamesgeorgehalt@gmail.com>
2025-10-21 13:22:34 -0400

    working on Admin/Manager login logic

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/DAO/ManagerDAO.java
@@ -0,0 +1,10 @@
+package com.ufund.api.ufundapi.DAO;
+
+import java.io.IOException;
+
+import com.ufund.api.ufundapi.Model.Manager;
+
+public interface ManagerDAO {
+    Manager getManager() throws IOException;
+
+}
