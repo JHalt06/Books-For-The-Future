8e3e128e8616a26c124943b146f63b02ffbff7ef
JHalt06 <jamesgeorgehalt@gmail.com>
2025-10-21 13:46:10 -0400

    admin/manager login ready for testing

--- ufund-api/src/main/java/com/ufund/api/ufundapi/Service/ManagerService.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/Service/ManagerService.java
@@ -2,10 +2,12 @@ package com.ufund.api.ufundapi.Service;
 
 import java.io.IOException;
 
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import com.ufund.api.ufundapi.DAO.FileInventoryDAO;
 import com.ufund.api.ufundapi.DAO.InventoryDAO;
+import com.ufund.api.ufundapi.DAO.ManagerDAO;
+import com.ufund.api.ufundapi.Model.Manager;
 import com.ufund.api.ufundapi.Model.Need;
 
 @Service
@@ -13,9 +15,25 @@ public class ManagerService { // also Developer/Admin?
     //developer CRUD operations
     // private final CupboardDAO cupboardDao = new FileCupboardDAO();
     private final InventoryDAO inventoryDao = new FileInventoryDAO();
+    private final ManagerDAO managerDao;
     
     // all CRUD operations are contained in this file, but the logic is held in the Cupboard/Inventory.java model files
     // not sure if saveCupboard() and saveInventory() need to be in here
+
+    public ManagerService(ManagerDAO managerDao){
+        this.managerDao = managerDao;
+    }
+
+    public boolean validateManagerLogin(String username, String password){
+        try{ 
+            Manager manager = managerDao.getManager(); //gets the manager info from the JSON data through calling the FileManagerDAO.getManager() method
+            return manager.getUsername().equals(username) && manager.getPassword().equals(password); 
+        }
+        catch (IOException e){
+            return false;
+        }
+    }
+
 
     public Need findNeedById(String id) {
         return inventoryDao.getNeedByID(id);
