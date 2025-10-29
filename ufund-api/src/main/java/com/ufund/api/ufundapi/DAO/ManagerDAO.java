1a7206f45b71c4b373bffc9225f4bd03299b5c24
JHalt06 <jamesgeorgehalt@gmail.com>
2025-10-29 17:35:45 -0400

    Remove unneeded Manager-related files

--- ufund-api/src/main/java/com/ufund/api/ufundapi/DAO/ManagerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ufund.api.ufundapi.DAO;
-
-import java.io.IOException;
-
-import com.ufund.api.ufundapi.Model.Manager;
-
-public interface ManagerDAO {
-    Manager getManager() throws IOException;
-}
