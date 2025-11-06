f890c94ec7bc63343ddf302c7fe4fd3097de75b3
tommy-bell-2 <tjb435tjb@gmail.com>
2025-11-06 16:03:11 -0500

    Stats should now work

--- ufund-api/src/main/java/com/ufund/api/ufundapi/Controller/DashboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/Controller/DashboardController.java
@@ -1,2 +1,38 @@
-package com.ufund.api.ufundapi.Controller;public class DashboardController {
+
+package com.ufund.api.ufundapi.Controller;
+
+import com.ufund.api.ufundapi.Model.DashboardStats;
+import com.ufund.api.ufundapi.Service.DashboardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    private static final Logger LOG = Logger.getLogger(DashboardController.class.getName());
+    private final DashboardService dashboardService;
+
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<DashboardStats> getStats() {
+        LOG.info("GET /dashboard/stats");
+        try {
+            DashboardStats stats = dashboardService.getDashboardStats();
+            return new ResponseEntity<>(stats, HttpStatus.OK);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- ufund-ui/ufund-ui/src/app/components/manager-dashboard/manager-dashboard.component.ts
+++ ufund-ui/ufund-ui/src/app/components/manager-dashboard/manager-dashboard.component.ts
@@ -1,45 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../../services/auth.service';
-import {Router} from '@angular/router';
-import {CupboardService} from '../../services/cupboard.service';
-import {UsersService} from '../../services/users.service';
-import {BehaviorSubject} from 'rxjs';
-import {Need} from '../../models/need.model';
-import {UserRole} from '../../models/user.model';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UserRole } from '../../models/user.model';
+import { DashboardService } from '../../services/dashboard.service';
+import { DashboardStats } from '../../models/dashboard-stats.model';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-dashboard',
-    standalone: false,
-    templateUrl: './manager-dashboard.component.html',
-    styleUrl: './manager-dashboard.component.css'
+  selector: 'app-manager-dashboard',
+  standalone: false, // <-- Ensure this is set to false
+  templateUrl: './manager-dashboard.component.html',
+  styleUrls: ['./manager-dashboard.component.css']
 })
-export class ManagerDashboardComponent implements OnInit{
+export class ManagerDashboardComponent implements OnInit {
 
-    protected userCount = new BehaviorSubject<number | undefined>(undefined)
-    protected totalFunding = new BehaviorSubject<String | undefined>(undefined)
-    protected totalNeeds = new BehaviorSubject<number | undefined>(undefined)
-    protected completedNeeds = new BehaviorSubject<Need[] | undefined>(undefined)
-    role: typeof UserRole = UserRole
+  stats$?: Observable<DashboardStats>;
+  protected readonly role: typeof UserRole = UserRole;
 
-    constructor(
-        protected authService: AuthService,
-        protected router: Router,
-        protected cupboardService: CupboardService,
-        protected userService: UsersService
-    ) {}
+  constructor(
+    protected authService: AuthService,
+    private dashboardService: DashboardService
+  ) {}
 
-    ngOnInit() {
-        this.cupboardService.getNeeds().subscribe(needs => {
-            let totalValue = 0
-            for (let need of needs) {
-              totalValue += need.fundingAmount
-              this.totalFunding.next(totalValue.toLocaleString())
-            }
-            this.completedNeeds.next(needs.filter(a => ((a.fundingAmount / a.quantity)) >= 1))
-            this.totalNeeds.next(needs.length)
-            needs = needs.filter(a => a.fundingAmount != 0)
-        })
+  ngOnInit(): void {
+    // Only fetch stats if the user is a manager
+    if (this.authService.getCurrentUser()?.role === UserRole.MANAGER) {
+      this.stats$ = this.dashboardService.getStats();
     }
-
-    protected readonly UserRole = UserRole;
+  }
 }
